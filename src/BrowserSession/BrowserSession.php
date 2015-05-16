6c26015af37ec4bbbfe60cd4aa24ef854d52fe43
Gabriel Bull <me@gabrielbull.com>
2015-05-16 15:32:33 -0400

    Removed custom database driver for browser session

--- src/BrowserSession/BrowserSession.php
+++ src/BrowserSession/BrowserSession.php
@@ -5,6 +5,7 @@ namespace Smart\Core\BrowserSession;
 use Sinergi\Container\ContainerInterface;
 use Smart\BrowserSession\BrowserSessionController;
 use Smart\BrowserSession\CacheDriver\PredisDriver;
+use Smart\BrowserSession\DatabaseDriver\DoctrineDriver;
 use Smart\BrowserSession\RouterDriver\SmartDriver;
 use Smart\Core\Container;
 
@@ -21,9 +22,11 @@ class BrowserSession
     public function __construct(ContainerInterface $container)
     {
         $this->controller = new BrowserSessionController(
-            new DatabaseDriver($container),
-            new SmartDriver($container->getRouter(), $container->getRequest(),
-                $container->getResponse()),
+            new DoctrineDriver($container->getEntityManager()),
+            new SmartDriver(
+                $container->getRouter(), $container->getRequest(),
+                $container->getResponse()
+            ),
             new PredisDriver($container->getPredis()->getClient())
         );
     }
